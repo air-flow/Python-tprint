254e41d70605067f8237d0619dc49131c0b35f0d
辻気流 <kiryu.18011106@gmail.com>
2020-10-23 07:35:27 +0900

    構想段階

---
+++ src/tutorial/result.py
@@ -0,0 +1,10 @@
+import datetime
+
+
+result = [
+    {'create_at': datetime.datetime(2020, 10, 8, 15, 8, 43), 'user_id': 1},
+    {'create_at': datetime.datetime(2020, 10, 8, 15, 8, 50), 'user_id': 2},
+    {'create_at': datetime.datetime(2020, 10, 8, 15, 9, 10), 'user_id': 3},
+    {'create_at': datetime.datetime(2020, 10, 8, 15, 13, 45), 'user_id': 4},
+    {'create_at': datetime.datetime(2020, 10, 8, 15, 17, 59), 'user_id': 5}
+]
