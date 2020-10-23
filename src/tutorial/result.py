10660778ef27b7e0c66049d29cd33a37959e24fb
辻気流 <kiryu.18011106@gmail.com>
2020-10-23 10:37:30 +0900

    keyを含めた値から一番長い文字数を取得関数作成

--- src/tutorial/result.py
+++ src/tutorial/result.py
@@ -1,10 +1,43 @@
 import datetime
+import pprint
+import random
+def tprint(data):
+    column = data[0].keys()
+    print(column)
+    for i in data:
+        # print(list(i.values()))
+        # i[0].rjust(MoreStrLne(i), ' ')
+        # print("|".join(map(str,list(i.values()))))
+        pass
+def MoreStrLne(data):
+    # data = list(range(1,100,1))
+    # return max(data,key=len())
+    # print(data)
+    return len(max(map(str,data),key=len))
 
+def GetValueList(key,data):
+    temp = [i[key] for i in data]
+    return temp
 
-result = [
-    {'create_at': datetime.datetime(2020, 10, 8, 15, 8, 43), 'user_id': 1},
-    {'create_at': datetime.datetime(2020, 10, 8, 15, 8, 50), 'user_id': 2},
-    {'create_at': datetime.datetime(2020, 10, 8, 15, 9, 10), 'user_id': 3},
-    {'create_at': datetime.datetime(2020, 10, 8, 15, 13, 45), 'user_id': 4},
-    {'create_at': datetime.datetime(2020, 10, 8, 15, 17, 59), 'user_id': 5}
-]
+def GetColumnValue(data):
+    key_list = data[0].keys()
+    result = []
+    for i in key_list:
+        temp = GetValueList(i,data)
+        temp.append(i)
+        result.append(MoreStrLne(temp))
+    return result
+def CreateData():
+    result = []
+    for i in range(1,11,1):
+        temp = {"id":i,"name":"","crrent_datetime_date_time_today_":datetime.date.today().strftime('%Y/%m/%d')}
+        name = ["a","ab","abc","abcd","abcde"]
+        temp["name"] = random.choices(name,k=1)[0]
+        result.append(temp)
+    return result
+if __name__ == "__main__":
+    # MoreStrLne()
+    # GetValueList("id",)
+    data = CreateData()
+    max_len = GetColumnValue(data)
+    print(GetColumnValue(data))
